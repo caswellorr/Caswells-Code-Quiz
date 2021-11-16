22100e3c4043667d0945c2f8f9b25a4a23c16686
Caswell Orr <caswell.orr@gmail.com>
2021-11-15 18:40:05 -0600

    updated questions and answers. cut reset button

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
 //  ================== TIMER =========================
 
-let timeLeft = 20;
-let startButton = document.querySelector(".start-button")
+let timeLeft = 60;
+let startButton = document.querySelector("#start-button");
 let intervalId;
-let displayTimer = document.querySelector(".timer-count");
+let displayTimer = document.querySelector("#countdown");
+let resetButton = document.querySelector("#reset-button");
+
+
 startButton.addEventListener("click", function(){
   clearInterval(intervalId);
-  timeLeft = 10;
+  timeLeft = 60;
 
    
   startTimer();
 
-})
+
+});
 
 function startTimer(){
 
@@ -27,45 +31,52 @@ function startTimer(){
     
     }, 1000)
 
-}
+};
+
+
 
 // ================== QUESTIONS ======================
 
-
+let currentQuestion = 0;
 let questions = [
   {
-    question: "What is the capitol of Texas?",
-    answers: ["Houston", "Dallas", "San Antonio", "Austin"],
-    correctAnswer: 3,
-  },
-  {
-    question: "What is the capitol of Colorado?",
-    answers: ["Denver", "Carbondale", "Estes Park", "Boulder"],
+    question: "All my exes live in ________ ...",
+    answers: ["Texas", "Kansas", "Sturgis", "Frisco"],
     correctAnswer: 0,
   },
   {
-    question: "What is the capitol of California?",
-    answers: ["Los Angeles", "San Francisco", "Sacramento", "San Diego"],
+    question: "the Rocky Mountain ________ is better than the way we had...",
+    answers: ["clay", "hey", "way", "stay"],
+    correctAnswer: 2,
+  },
+  {
+    question: "Got me on my knees, _________ ...",
+    answers: ["Rigby", "Perla", "Layla", "Kayla"],
+    correctAnswer: 2,
+  },
+  {
+    question: "__________ all my troubles seemed so far away...",
+    answers: ["In the car today", "The other day", "Wednesday", "Yesterday"],
     correctAnswer: 3,
   },
   {
-    question: "What is the capitol of South Dakota?",
-    answers: ["Sturgis", "Pierre", "Rapid City", "Deadwood"],
-    correctAnswer: 2,
+    question: "__________ on the Mountain ...",
+    answers: ["Aspens", "Fire", "Snow", "Goats"],
+    correctAnswer: 1,
   },
 ];
 
-// console.log(questions);
 
-let currentQuestion = 0
+// questions = questions[currentQuestion];
 
-for (let i=0; i < questions.length ; i++) {
-  const element = questions[i]
-}
+// for (let i=0; i < questions.length ; i++) {
+//   const element = questions[i];
+//   console.log(currentQuestion);
+// }
 
-for (let j = 0; j < questions[startingQuestinos].answers.length; i++);
+// for (let j = 0; j < questions[currentQuestion].answers.length; i++);
 
-document.querySelector("#question").textContent = questions
+// document.querySelector("#question").textContent = questions
 
 
-
+// document.querySelector("#question").textContent = questions[currentQuestion].question;
